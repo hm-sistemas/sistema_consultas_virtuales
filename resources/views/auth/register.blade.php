faf06414614887d0c16e515619ce155acc865abe
HM Edgar <hospmex.sistemas@gmail.com>
2021-01-02 09:51:54 -0800

    auth buttons

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -101,20 +101,25 @@
                     <x-label for="titles" :value="__('Títulos')" />
 
                     <x-input id="titles" class="block mt-1 w-full" type="text" name="titles" :value="old('titles')"
-                        placeholder="Dentista, Medico, etc..." />
+                        placeholder="Dentista, Médico, etc..." />
                 </div>
             </fieldset>
 
-
-
-            <div class="flex items-center justify-end mt-4">
-                {{-- <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-                </a> --}}
-
-                <x-button class="ml-4">
-                    {{ __('Registrar') }}
-                </x-button>
+            <div class="mt-4">
+                <button type="submit"
+                    class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <span class="absolute left-0 inset-y-0 flex items-center pl-3">
+                        <!-- Heroicon name: lock-closed -->
+                        <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
+                            aria-hidden="true">
+                            <path fill-rule="evenodd"
+                                d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                    Registrar
+                </button>
             </div>
         </form>
     </x-auth-card>
